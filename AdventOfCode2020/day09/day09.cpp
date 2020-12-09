67032e4eba9e380e2ea51a37bea23350e4e7459a
Dave Knight <trave.uk@gmail.com>
2020-12-09 20:56:29 +0000

    2020 day 9

--- AdventOfCode2020/day09/day09.cpp
+++ AdventOfCode2020/day09/day09.cpp
@@ -3,23 +3,104 @@
 
 #include "stdafx.h"
 
-int main()
+using Value = __int64;
+
+int process(const char *filename, int preamble)
 {
-	int result = 0;
+	std::vector<Value> numbers;
+	FILE *fp = fopen(filename, "rt");
 	char* buffer = new char[65536];
-	FILE *fp = fopen( "example.txt", "rt" );
-//	FILE *fp = fopen( "input.txt", "rt" );
-	while ( !feof( fp ) )
+	while (!feof(fp))
 	{
-		char* thisLine = fgets( buffer, 65536, fp );
-		if ( thisLine )
+		char* thisLine = fgets(buffer, 65536, fp);
+		if (thisLine)
 		{
-			thisLine[strcspn( thisLine, "\n\r" )] = '\0';
+			thisLine[strcspn(thisLine, "\n\r")] = '\0';
+			if (*thisLine)
+			{
+				numbers.push_back(atoll(thisLine));
+			}
 		}
 	}
-	fclose( fp );
+	fclose(fp);
 	delete[] buffer;
 
-	printf( "result: %d\n", result );
+	std::vector<int> candidates;
+	for (int i = 0; i < preamble; ++i)
+	{
+		for (int j = 0; j < preamble; ++j)
+		{
+			if (i == j)
+				continue;
+			candidates.push_back(numbers[i] + numbers[j]);
+		}
+	}
+
+	Value part1 = -1;
+	for (int i = preamble; i < numbers.size(); ++i)
+	{
+		if (i > preamble)
+		{
+			// now update candidates
+			const int oldIndex = i - 1 - preamble;
+			const Value old = numbers[oldIndex];
+			const Value diff = numbers[i - 1] - old;
+			for (int j = 1; j < preamble; ++j)
+			{
+				Value sum = old + numbers[oldIndex + j];
+				auto x = std::find(candidates.begin(), candidates.end(), sum);
+				assert(x != candidates.end());
+				*x += diff;
+			}
+		}
+
+		auto x = std::find(candidates.begin(), candidates.end(), numbers[i]);
+		if (x == candidates.end())
+		{
+			part1 = numbers[i];
+			printf("Part 1: %lld\n", part1);
+			break;
+		}
+	}
+
+	for (int i = 0; i < numbers.size(); ++i)
+	{
+		if (numbers[i] >= part1)
+			continue;
+
+		Value sum = 0;
+		int j;
+		for (j = i; j < numbers.size() && sum < part1; ++j)
+		{
+			sum += numbers[j];
+		}
+		if (sum == part1)
+		{
+			Value lowest = part1;
+			Value highest = 0;
+			for (int k = i; k < j; ++k)
+			{
+				if (numbers[k] > highest)
+				{
+					highest = numbers[k];
+				}
+				if (numbers[k] < lowest)
+				{
+					lowest = numbers[k];
+				}
+			}
+			Value part2 = highest + lowest;
+			printf("Part 2: %lld\n", part2);
+		}
+	}
+
+	return -1;
+}
+
+int main()
+{
+//	process("example.txt", 5);
+	process("input.txt", 25);
+
 	return 0;
 }
