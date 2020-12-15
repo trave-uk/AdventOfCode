46c41a0e02b50a094330024f09e1da0d02187cf0
Dave Knight <trave.uk@gmail.com>
2020-12-15 11:46:31 +0000

    AoC 2020 day 15. Process() is slow, Process2() is faster.

--- AdventOfCode2020/day15/day15.cpp
+++ AdventOfCode2020/day15/day15.cpp
@@ -3,23 +3,89 @@
 
 #include "stdafx.h"
 
+void Process(std::vector<int> input)
+{
+	std::vector<int> output = input;
+	while (output.size() < 2020)
+	{
+		int last = output.back();
+		int count = 0;
+		for (auto iter = output.rbegin(); iter != output.rend(); ++iter)
+		{
+			if (count > 0 && *iter == last)
+			{
+				break;
+			}
+			++count;
+		}
+		if (count == output.size())
+			count = 0;
+		output.push_back(count);
+	}
+	std::string stringInput;
+	for (int i : input)
+	{
+		stringInput += std::to_string(i);
+		stringInput += ',';
+	}
+	printf("  - Given the starting numbers %s the 2020th number spoken is %d.\n", stringInput.data(), output.back());
+}
+
+void Process2(std::vector<__int64> input, __int64 target = 2020L)
+{
+	std::map<__int64, __int64> seen;
+	__int64 ix = 0;
+	for (__int64 i : input)
+	{
+		if (ix < input.size() - 1)
+			seen[i] = ix;
+		++ix;
+	}
+	__int64 number = input.back();
+	while (ix < target)
+	{
+		__int64 newnum;
+		if (!seen.count(number))
+		{
+			newnum = 0;
+		}
+		else
+		{
+			newnum = ix - 1 - seen[number];
+		}
+		seen[number] = ix-1;
+		++ix;
+		number = newnum;
+//		if (count > max)
+//			max = count;
+	}
+	std::string stringInput;
+	for (__int64 i : input)
+	{
+		stringInput += std::to_string(i);
+		stringInput += ',';
+	}
+	printf("  - Given %s the %lldth number spoken is %lld.\n", stringInput.data(), target, number);
+}
+
 int main()
 {
-	int result = 0;
-	char* buffer = new char[65536];
-	FILE *fp = fopen( "example.txt", "rt" );
-//	FILE *fp = fopen( "input.txt", "rt" );
-	while ( !feof( fp ) )
-	{
-		char* thisLine = fgets( buffer, 65536, fp );
-		if ( thisLine )
-		{
-			thisLine[strcspn( thisLine, "\n\r" )] = '\0';
-		}
-	}
-	fclose( fp );
-	delete[] buffer;
-
-	printf( "result: %d\n", result );
-	return 0;
+	Process2({ 0,3,6 });
+	Process2({ 1,3,2 });
+	Process2({ 2,1,3 });
+	Process2({ 1,2,3 });
+	Process2({ 2,3,1 });
+	Process2({ 3,2,1 });
+	Process2({ 3,1,2 });
+	Process2({ 13,16,0,12,15,1 });
+	printf("\n");
+	__int64 target = 30000000L;
+	Process2({ 0,3,6 }, target);
+	Process2({ 1,3,2 }, target);
+	Process2({ 2,1,3 }, target);
+	Process2({ 1,2,3 }, target);
+	Process2({ 2,3,1 }, target);
+	Process2({ 3,2,1 }, target);
+	Process2({ 3,1,2 }, target);
+	Process2({ 13,16,0,12,15,1 }, target);
 }
