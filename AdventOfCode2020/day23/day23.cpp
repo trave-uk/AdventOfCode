7a90de94e8d6c09f550883c2600f090890e602b7
Dave Knight <trave.uk@gmail.com>
2020-12-23 12:48:21 +0000

    AoC 2020 day 23

--- AdventOfCode2020/day23/day23.cpp
+++ AdventOfCode2020/day23/day23.cpp
@@ -3,23 +3,102 @@
 
 #include "stdafx.h"
 
+using Circle = std::map<int, int>;
+Circle circle;
+
+void Process(const char* input, int turns, int part)
+{
+	// Initialise the circle
+	circle.clear();
+	int minvalue = INT_MAX;
+	int maxvalue = 0;
+	std::vector<int> inputs;
+	for (const char *c = input; *c; ++c)
+	{
+		int n = *c - '0';
+		inputs.push_back(n);
+		if (n > maxvalue)
+			maxvalue = n;
+		if (n < minvalue)
+			minvalue = n;
+	}
+	int lastValue = 0;
+	for (int i = 0; i < inputs.size(); ++i)
+	{
+		circle[inputs[i]] = inputs[(i + 1) % inputs.size()];
+		lastValue = inputs[i];
+	}
+
+	if (part == 2)
+	{
+		// increase size to a million entries
+		while (maxvalue < 1000000)
+		{
+			++maxvalue;
+			circle[lastValue] = maxvalue;
+			lastValue = maxvalue;
+		}
+		circle[lastValue] = inputs[0];
+	}
+	
+	int value = inputs[0];
+	for (int t = 0; t < turns; ++t)
+	{
+		std::set<int> tset;
+		// remove 3 pieces clockwise from current position
+		int next = circle[value];
+		int x = next;
+		int last;
+		for (int i = 0; i < 3; ++i)
+		{
+			tset.insert(x);
+			last = x;
+			x = circle[x];
+		}
+		circle[value] = x;
+		// choose a lower value
+		int newvalue = value;
+		while (newvalue == value || tset.count(newvalue))
+		{
+			--newvalue;
+			if (newvalue < minvalue)
+				newvalue = maxvalue;
+		}
+		// insert 3 pieces after this value
+		circle[last] = circle[newvalue];
+		circle[newvalue] = next;
+		// next position
+		value = circle[value];
+	}
+
+	if (part == 1)
+	{
+		// split circle at '1'
+		std::string result;
+		int v = circle[1];
+		while (v != 1)
+		{
+			result.push_back(v + '0');
+			v = circle[v];
+		}
+		printf("Part 1: input %s, %d turns, output %s\n", input, turns, result.data());
+	}
+	else
+	{
+		int mult = 0;
+		int v = circle[1];
+		int64 result = v;
+		result *= circle[v];
+		printf("Part 2: input %s, %d turns, output %d*%d= %lld\n", input, turns, v, circle[v], result);
+	}
+}
+
 int main()
 {
-	int result = 0;
-	char* buffer = new char[65536];
-	FILE *fp = fopen( "example.txt", "rt" );
-//	FILE *fp = fopen( "input.txt", "rt" );
-	while ( !feof( fp ) )
-	{
-		char* thisLine = fgets( buffer, 65536, fp );
-		if ( thisLine )
-		{
-			thisLine[strcspn( thisLine, "\n\r" )] = '\0';
-		}
-	}
-	fclose( fp );
-	delete[] buffer;
+	Process("389125467", 10, 1);
+	Process("389125467", 100, 1);
+	Process("167248359", 100, 1); // puzzle input
 
-	printf( "result: %d\n", result );
-	return 0;
+	Process("389125467", 10000000, 2);
+	Process("167248359", 10000000, 2); // puzzle input
 }
