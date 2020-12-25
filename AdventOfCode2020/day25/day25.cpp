5fd0c3d1c0d3f222263394b183b33dc54af7f38c
Dave Knight <trave.uk@gmail.com>
2020-12-25 06:12:58 +0000

    AoC 2020 day 25 :)

--- AdventOfCode2020/day25/day25.cpp
+++ AdventOfCode2020/day25/day25.cpp
@@ -3,23 +3,62 @@
 
 #include "stdafx.h"
 
+
+int transform(int subject, int privateX)
+{
+	int64 i = 1;
+	for (int step = 0; step < privateX; ++step)
+	{
+		i *= subject;
+		i %= 20201227;
+	}
+	return (int)i;
+}
+
+int Calculate(int public1, int public2)
+{
+	int i = 1;
+	int private1 = -1;
+	int private2 = -1;
+	for (int step = 0; private1 == -1 || private2 == -1; ++step)
+	{
+		i *= 7;
+		i %= 20201227;
+
+		if (i == public1)
+			private1 = step+1;
+		if (i == public2)
+			private2 = step+1;
+	}
+
+	int key1 = transform(public1, private2);
+	int key2 = transform(public2, private1);
+	assert(key1 == key2);
+	return key1;
+}
+
 int main()
 {
-	int result = 0;
+	std::vector<int> keys;
+
 	char* buffer = new char[65536];
-	FILE *fp = fopen( "example.txt", "rt" );
-//	FILE *fp = fopen( "input.txt", "rt" );
+	//FILE *fp = fopen( "example.txt", "rt" );
+	FILE *fp = fopen( "input.txt", "rt" );
 	while ( !feof( fp ) )
 	{
 		char* thisLine = fgets( buffer, 65536, fp );
 		if ( thisLine )
 		{
 			thisLine[strcspn( thisLine, "\n\r" )] = '\0';
+			if (*thisLine)
+				keys.push_back(atoi(thisLine));
 		}
 	}
 	fclose( fp );
 	delete[] buffer;
 
-	printf( "result: %d\n", result );
+	assert(keys.size() == 2);
+
+	printf("result: %d\n", Calculate(keys[0], keys[1]));
 	return 0;
 }
