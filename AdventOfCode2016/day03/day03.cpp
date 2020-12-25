f8e90140cc29894aba4ec540c69cb06ae3e1f412
trave <trave.uk@gmail.com>
2020-12-25 20:12:33 +0000

    AoC 2016 day 3

--- AdventOfCode2016/day03/day03.cpp
+++ AdventOfCode2016/day03/day03.cpp
@@ -6,10 +6,12 @@
 
 int main()
 {
-	int result = 0;
+	int part1 = 0;
+	int part2 = 0;
 	char* buffer = new char[65536];
-	FILE *fp = fopen( "example.txt", "rt" );
-	//FILE *fp = fopen( "input.txt", "rt" );
+	//FILE *fp = fopen( "example.txt", "rt" );
+	FILE *fp = fopen( "input.txt", "rt" );
+	std::vector<std::vector<int>> tris;
 	while ( !feof( fp ) )
 	{
 		char* thisLine = fgets( buffer, 65536, fp );
@@ -18,13 +20,48 @@ int main()
 			thisLine[strcspn( thisLine, "\n\r" )] = '\0';
 			if ( *thisLine )
 			{
-
+				std::vector<int> tri;
+				char* tok = strtok( thisLine, " " );
+				while ( tok )
+				{
+					assert( isdigit( *tok ) );
+					int side = atoi( tok );
+					tri.push_back( side );
+					tok = strtok( nullptr, " " );
+				}
+				assert( tri.size() == 3 );
+				tris.push_back( tri );
+				std::sort( tri.begin(), tri.end() );
+				if ( tri[0] + tri[1] > tri[2] )
+				{
+					++part1;
+				}
+				if ( tris.size() == 3 )
+				{
+					for ( int i = 0; i < 3; ++i )
+					{
+						tri.clear();
+						for ( int j = 0; j < 3; ++j )
+						{
+							tri.push_back( tris[j][i] );
+						}
+						assert( tri.size() == 3 );
+						std::sort( tri.begin(), tri.end() );
+						if ( tri[0] + tri[1] > tri[2] )
+						{
+							++part2;
+						}
+					}
+					tris.clear();
+				}
 			}
 		}
 	}
 	fclose( fp );
 	delete[] buffer;
 
-	printf( "Part 1: %d\n", result );
+	printf( "Part 1: %d\n", part1 );
+	printf( "Part 2: %d\n", part2 );
 	return 0;
 }
+// 1266 too low
