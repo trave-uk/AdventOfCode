59dac3446181d97c4b8e3f940ce5559aee8b2a2d
trave <trave.uk@gmail.com>
2020-12-25 18:18:22 +0000

    AoC 2016 day 1

--- AdventOfCode2016/day01/day01.cpp
+++ AdventOfCode2016/day01/day01.cpp
@@ -4,12 +4,26 @@
 
 #include "stdafx.h"
 
+struct Coord
+{
+	Coord() = default;
+	Coord( int x_, int y_ ) : x( x_ ), y( y_ ) {}
+	bool operator<( const Coord& other ) const { return x < other.x || ( x == other.x && y < other.y ); }
+	int len() { return abs( x ) + abs( y ); }
+	int x;
+	int y;
+};
+
 int main()
 {
-	int result = 0;
 	char* buffer = new char[65536];
-	FILE *fp = fopen( "example.txt", "rt" );
-	//FILE *fp = fopen( "input.txt", "rt" );
+	//FILE *fp = fopen( "example.txt", "rt" );
+	//FILE *fp = fopen( "example2.txt", "rt" );
+	FILE *fp = fopen( "input.txt", "rt" );
+	Coord pos(0,0);
+	Coord dir(0,1);
+	Coord dest(0,0);
+	std::set<Coord> history;
 	while ( !feof( fp ) )
 	{
 		char* thisLine = fgets( buffer, 65536, fp );
@@ -18,13 +32,57 @@ int main()
 			thisLine[strcspn( thisLine, "\n\r" )] = '\0';
 			if ( *thisLine )
 			{
+				char* tok = strtok( thisLine, ", " );
+				while ( tok )
+				{
+					int dist = atoi( tok+1 );
+					switch ( *tok )
+					{
+						case 'R':
+						{
+							int d = dir.y;
+							dir.y = -dir.x;
+							dir.x = d;
+							break;
+						}
+						case 'L':
+						{
+							int d = dir.y;
+							dir.y = dir.x;
+							dir.x = -d;
+							break;
+						}
+						default:
+						{
+							assert( false );
+						}
+					}
+					for ( int step = 0; step < dist; ++step )
+					{
+						pos.x += dir.x;
+						pos.y += dir.y;
 
+						if ( dest.len() == 0 )
+						{
+							if ( history.count( pos ) )
+							{
+								dest = pos;
+							}
+							else
+							{
+								history.insert( pos );
+							}
+						}
+					}
+					tok = strtok( nullptr, ", " );
+				}
 			}
 		}
 	}
 	fclose( fp );
 	delete[] buffer;
 
-	printf( "Part 1: %d\n", result );
+	printf( "Part 1: %d\n", pos.len() );
+	printf( "Part 2: %d\n", dest.len() );
 	return 0;
 }
