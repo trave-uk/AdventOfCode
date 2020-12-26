93c79d71da6d788ca9b7d4ae1b83d0828eab2528
trave <trave.uk@gmail.com>
2020-12-26 00:33:55 +0000

    AoC 2016 solution, projects and stub files

---
+++ AdventOfCode2016/libheaders.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <stdio.h>
+#include <tchar.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <basetsd.h>
+#include <corecrt_math_defines.h>
+#include <corecrt_malloc.h>
+#include <set>
+#include <map>
+#include <array>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <list>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <deque>
+#include <bitset>
+
+#include <windows.h>
+#include <processenv.h>
+#include <winnt.h>
+#include <wincon.h>
+#include <conio.h>
+
+#define _ARRAYSIZE_(x) (sizeof(x)/sizeof(*x))
+
+#include "crc.h"
+
+using int64 = __int64;
