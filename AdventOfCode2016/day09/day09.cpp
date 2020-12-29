32d29ac68050766bc92ddfb011f89c72dd1457a1
trave <trave.uk@gmail.com>
2020-12-29 21:29:33 +0000

    AoC 2016 day 9

--- AdventOfCode2016/day09/day09.cpp
+++ AdventOfCode2016/day09/day09.cpp
@@ -1,30 +1,65 @@
 // day09.cpp : Advent of Code 2016 https://adventofcode.com/2016
-// Solution by trave.uk 25/12/2020 https://github.com/trave-uk/AdventOfCode
+// Solution by trave.uk 29/12/2020 https://github.com/trave-uk/AdventOfCode
 //
 
 #include "stdafx.h"
+
+int64 calculateLength( std::string input, int part )
+{
+	int64 length = 0;
+	const char* pos = input.data();
+	while ( *pos )
+	{
+		int num = 0, repeat = 0;
+		if ( sscanf( pos, "(%dx%d)", &num, &repeat ) == 2 )
+		{
+			pos += strcspn( pos, ")" ) + 1;
+			if ( part == 1 )
+			{
+				length += num * repeat;
+			}
+			else
+			{
+				std::string segment = std::string( pos ).substr( 0, num );
+				int64 sublen = calculateLength( segment, part );
+				length += sublen * repeat;
+			}
+			pos += num;
+		}
+		else
+		{
+			++length;
+			++pos;
+		}
+	}
+	return length;
+}
 
 int main()
 {
 	int result = 0;
 	char* buffer = new char[65536];
-	FILE *fp = fopen( "example.txt", "rt" );
-	//FILE *fp = fopen( "input.txt", "rt" );
-	while ( !feof( fp ) )
+	for ( const char* filename : std::vector<const char*>( { "example.txt", "input.txt" } ) )
 	{
-		char* thisLine = fgets( buffer, 65536, fp );
-		if ( thisLine )
+		printf( "File %s...\n", filename );
+		FILE *fp = fopen( filename, "rt" );
+		while ( !feof( fp ) )
 		{
-			thisLine[strcspn( thisLine, "\n\r" )] = '\0';
-			if ( *thisLine )
+			char* thisLine = fgets( buffer, 65536, fp );
+			if ( thisLine )
 			{
-
+				thisLine[strcspn( thisLine, "\n\r" )] = '\0';
+				if ( *thisLine )
+				{
+					std::string input( thisLine );
+					printf( "%s... ->\n", input.substr( 0, 32 ).data() );
+					printf( " Part 1: %zd\n", calculateLength( input, 1 ) );
+					printf( " Part 2: %zd\n", calculateLength( input, 2 ) );
+				}
 			}
 		}
+		fclose( fp );
 	}
-	fclose( fp );
 	delete[] buffer;
-
-	printf( "Part 1: %d\n", result );
 	return 0;
 }
