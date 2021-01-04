0a0958af6f9f785aa890a041c897496b00fe6971
trave <trave.uk@gmail.com>
2021-01-04 15:02:19 +0000

    AoC 2016 day 12. Didn't actually build an interpreter; just hand-generated equivalent C/C++ to the input.

--- AdventOfCode2016/day12/day12.cpp
+++ AdventOfCode2016/day12/day12.cpp
@@ -1,30 +1,57 @@
 // day12.cpp : Advent of Code 2016 https://adventofcode.com/2016
-// Solution by trave.uk 25/12/2020 https://github.com/trave-uk/AdventOfCode
+// Solution by trave.uk 04/01/2021 https://github.com/trave-uk/AdventOfCode
 //
 
 #include "stdafx.h"
 
+int algo(int c)
+{
+	int a=0, b=0, d=0;
+// cpy 1 a
+	a = 1;
+// cpy 1 b
+	b = 1;
+// cpy 26 d
+	d = 26;
+// jnz c 2 (_0)
+	if ( c )
+	{
+// jnz 1 5 (_1)
+// _0: cpy 7 c
+		d += 7;
+// _2: inc d
+// dec c
+// jnz c -2 (_2)
+	}
+
+	do 
+	{
+// _1: cpy a c
+		c = a;
+// _3: inc a
+// dec b
+// jnz b -2 (_3)
+		a += b;
+// cpy c b
+		b = c;
+// dec d
+		--d;
+// jnz d -6 (_1)
+	} while ( d );
+// cpy 18 c
+// _5: cpy 11 d
+// _4: inc a
+// dec d
+// jnz d -2 (_4)
+// dec c
+// jnz c -5 (_5)
+	a += 11 * 18;
+	return a;
+}
+
 int main()
 {
-	int result = 0;
-	char* buffer = new char[65536];
-	FILE *fp = fopen( "example.txt", "rt" );
-	//FILE *fp = fopen( "input.txt", "rt" );
-	while ( !feof( fp ) )
-	{
-		char* thisLine = fgets( buffer, 65536, fp );
-		if ( thisLine )
-		{
-			thisLine[strcspn( thisLine, "\n\r" )] = '\0';
-			if ( *thisLine )
-			{
-
-			}
-		}
-	}
-	fclose( fp );
-	delete[] buffer;
-
-	printf( "Part 1: %d\n", result );
+	printf( "Part 1: %d\n", algo( 0 ) );
+	printf( "Part 2: %d\n", algo( 1 ) );
 	return 0;
 }
