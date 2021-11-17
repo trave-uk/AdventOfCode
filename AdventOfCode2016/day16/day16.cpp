7007f2fc992b93ede117a64ac4996013d0cca513
Dave Knight <trave.uk@gmail.com>
2021-11-17 23:54:02 +0000

    AoC 2016 day 16

--- AdventOfCode2016/day16/day16.cpp
+++ AdventOfCode2016/day16/day16.cpp
@@ -1,30 +1,61 @@
 // day16.cpp : Advent of Code 2016 https://adventofcode.com/2016
-// Solution by trave.uk 25/12/2020 https://github.com/trave-uk/AdventOfCode
+// Solution by trave.uk 17/11/2021 https://github.com/trave-uk/AdventOfCode
 //
 
 #include "stdafx.h"
 
-int main()
+void test(const char* name, int length, std::string initial)
 {
-	int result = 0;
-	char* buffer = new char[65536];
-	FILE *fp = fopen( "example.txt", "rt" );
-	//FILE *fp = fopen( "input.txt", "rt" );
-	while ( !feof( fp ) )
+	std::string disk = initial;
+	disk.reserve(length);
+	
+	std::string copy;
+	copy.reserve(length);
+	while (disk.length() < length)
 	{
-		char* thisLine = fgets( buffer, 65536, fp );
-		if ( thisLine )
+		copy = disk;
+		disk.append("0");
+		for (std::string::reverse_iterator it = copy.rbegin(); it != copy.rend(); ++it)
 		{
-			thisLine[strcspn( thisLine, "\n\r" )] = '\0';
-			if ( *thisLine )
-			{
+			if (disk.length() == length)
+				break;
 
+			char c = *it;
+			if (c == '1')
+			{
+				disk.append("0");
+			}
+			else
+			{
+				disk.append("1");
 			}
 		}
 	}
-	fclose( fp );
-	delete[] buffer;
 
-	printf( "Part 1: %d\n", result );
+	while ((disk.length() % 2) == 0)
+	{
+		std::string next;
+		for (int i = 0; i < disk.length(); i += 2)
+		{
+			if (disk[i] == disk[i+1])
+			{
+				next.append("1");
+			}
+			else
+			{
+				next.append("0");
+			}
+		}
+		disk = next;
+	}
+	printf("%s: checksum: %s\n", name, disk.c_str());
+}
+
+int main()
+{
+	test("Example 1", 12, "110010110100");
+	test("Example 2", 20, "10000");
+	test("Part 1", 272, "00111101111101000");
+	test("Part 2", 35651584, "00111101111101000");
 	return 0;
 }
