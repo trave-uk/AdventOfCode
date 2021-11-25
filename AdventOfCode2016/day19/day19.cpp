e052fd10f9cbdac8761796413c6ec336af246bf0
trave <trave.uk@gmail.com>
2021-11-25 14:58:10 +0000

    AoC 2016 day 19. Part 2 very slow (ran overnight). Have some thoughts on quicker methods, but will revisit if I get time.

--- AdventOfCode2016/day19/day19.cpp
+++ AdventOfCode2016/day19/day19.cpp
@@ -4,27 +4,72 @@
 
 #include "stdafx.h"
 
+
+struct Elf
+{
+	Elf(int _number)
+		: number( _number )
+		, next( nullptr )
+	{}
+
+	int number;
+	Elf* next;
+};
+Elf& operator++( Elf& a )
+{
+	return *a.next;
+}
+
+void Process( std::string title, int numElves, bool bPart2 = false )
+{
+	std::vector<Elf> elves;
+	for ( int i = 1; i <= numElves; ++i )
+	{
+		elves.push_back( Elf( i ) );
+	}
+	for ( Elf& elf : elves )
+	{
+		elf.next = ( &elf ) + 1;
+	}
+	Elf& elf = elves.back();
+	elf.next = &elves[0];
+
+	int count = numElves;
+	Elf* pElf = &elves[0];
+	while ( pElf->next != pElf )
+	{
+		if ( bPart2 )
+		{
+			// Remove the opposite node. NB: count >= 2
+			Elf* pPrev = pElf;
+			int c = count / 2 - 1; 
+			while ( c > 0 )
+			{
+				pPrev = pPrev->next;
+				--c;
+			}
+			Elf* pNext = pPrev->next;
+			pPrev->next = pNext->next;
+		}
+		else
+		{
+			// Remove the next node
+			Elf* pNext = pElf->next;
+			pElf->next = pNext->next;
+		}
+		pElf = pElf->next;
+		--count;
+	}
+	printf( "%s Part %d: %d -> %d\n", title.c_str(), bPart2 ? 2 : 1, numElves, pElf->number );
+}
+
 int main()
 {
-	int result = 0;
-	char* buffer = new char[65536];
-	FILE *fp = fopen( "example.txt", "rt" );
-	//FILE *fp = fopen( "input.txt", "rt" );
-	while ( !feof( fp ) )
-	{
-		char* thisLine = fgets( buffer, 65536, fp );
-		if ( thisLine )
-		{
-			thisLine[strcspn( thisLine, "\n\r" )] = '\0';
-			if ( *thisLine )
-			{
+	Process( "Example", 5 );
+	Process( "My", 3012210 );
 
-			}
-		}
-	}
-	fclose( fp );
-	delete[] buffer;
+	Process( "Example", 5, true );
+	Process( "My", 3012210, true );
 
-	printf( "Part 1: %d\n", result );
 	return 0;
 }
