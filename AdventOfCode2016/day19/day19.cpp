047cacf60271f50269509209ff001e021c53576f
Dave Knight <trave.uk@gmail.com>
2021-11-25 16:54:46 +0000

    Corrected a date in a comment. Many of these are incorrect

--- AdventOfCode2016/day19/day19.cpp
+++ AdventOfCode2016/day19/day19.cpp
@@ -1,5 +1,5 @@
 // day19.cpp : Advent of Code 2016 https://adventofcode.com/2016
-// Solution by trave.uk 25/12/2020 https://github.com/trave-uk/AdventOfCode
+// Solution by trave.uk 22/11/2021 https://github.com/trave-uk/AdventOfCode
 //
 
 #include "stdafx.h"
