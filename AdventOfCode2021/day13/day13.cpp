7f2b5e79a0792456ffbccb0704d334565fd05bd1
Dave Knight <trave.uk@gmail.com>
2021-12-13 05:27:29 +0000

    AoC 2021 day 13

--- AdventOfCode2021/day13/day13.cpp
+++ AdventOfCode2021/day13/day13.cpp
@@ -6,9 +6,10 @@
 
 void Process(const char* filename)
 {
-	int result = 0;
+	std::set<coord> dots;
 	char* buffer = new char[65536];
 	FILE *fp = fopen(filename, "rt");
+	int64 part1 = 0;
 	while (!feof(fp))
 	{
 		char* thisLine = fgets(buffer, 65536, fp);
@@ -17,14 +18,88 @@ void Process(const char* filename)
 			thisLine[strcspn(thisLine, "\n\r")] = '\0';
 			if (*thisLine)
 			{
-
+				if (isdigit(*thisLine))
+				{
+					coord pos;
+					int matches = sscanf(thisLine, "%lld,%lld", &pos.first, &pos.second);
+					assert(matches == 2);
+					dots.insert(pos);
+				}
+				else
+				{
+					char axis;
+					int64 at;
+					int matches = sscanf(thisLine, "fold along %c=%lld", &axis, &at);
+					assert(matches == 2);
+					std::vector<std::pair<coord, coord>> moves;
+					for (coord pos : dots)
+					{
+						coord newPos = pos;
+						switch (axis)
+						{
+						case 'x':
+						{
+							if (pos.first > at)
+							{
+								newPos.first = at - (pos.first - at);
+							}
+							break;
+						}
+						case'y':
+						{
+							if (pos.second > at)
+							{
+								newPos.second = at - (pos.second - at);
+							}
+							break;
+						}
+						}
+						if (pos != newPos)
+						{
+							moves.push_back(std::make_pair(pos, newPos));
+						}
+					}
+					for (auto move : moves)
+					{
+						dots.erase(move.first);
+						dots.insert(move.second);
+					}
+					if (!part1)
+					{
+						part1 = dots.size();
+					}
+				}
 			}
 		}
 	}
 	fclose(fp);
 	delete[] buffer;
 
-	printf("%s: Part 1: %d\n", filename, result);
+	printf("%s: Part 1: %lld\n", filename, part1);
+
+	printf("%s: Part 2:\n", filename);
+	coord bottomRight;
+	for (coord pos : dots)
+	{
+		if (pos.first > bottomRight.first)
+			bottomRight.first = pos.first;
+		if (pos.second > bottomRight.second)
+			bottomRight.second = pos.second;
+	}
+
+	coord pos;
+	for (pos.second = 0; pos.second <= bottomRight.second; ++pos.second)
+	{
+		for (pos.first = 0; pos.first <= bottomRight.first; ++pos.first)
+		{
+			if (dots.count(pos))
+				_putch('#');
+			else
+				_putch(' ');
+		}
+		_putch('\n');
+	}
+	_putch('\n');
 }
 
 int main()
