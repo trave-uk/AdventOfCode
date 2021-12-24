cfbf160faceb8bada0badd67dec2e28c209dc762
Dave Knight <trave.uk@gmail.com>
2021-12-24 09:06:10 +0000

    AoC 2021 day 24.
    Note that rather than run the algorithm over all possible inputs, I manually worked out what the algorithm was doing to extract the rules for the digits, and then ran the process just to confirm that the answers I came up with were correct (though not that they were actually the maximum and minimum possible values!)
    Copious notes made in "input_notes.txt"

--- AdventOfCode2021/day24/day24.cpp
+++ AdventOfCode2021/day24/day24.cpp
@@ -4,11 +4,15 @@
 
 #include "stdafx.h"
 
-void Process(const char* filename)
+std::vector<std::string> assembly;
+
+bool load(const char* filename)
 {
-	int result = 0;
+	assembly.clear();
 	char* buffer = new char[65536];
 	FILE *fp = fopen(filename, "rt");
+	if (!fp)
+		return false;
 	while (!feof(fp))
 	{
 		char* thisLine = fgets(buffer, 65536, fp);
@@ -17,20 +21,121 @@ void Process(const char* filename)
 			thisLine[strcspn(thisLine, "\n\r")] = '\0';
 			if (*thisLine)
 			{
-
+				std::string line(thisLine);
+				assembly.push_back(line);
 			}
 		}
 	}
 	fclose(fp);
 	delete[] buffer;
+	return true;
+}
 
-	printf("%s: Part 1: %d\n", filename, result);
+
+#define CASE(x) (x[0] + x[1] * 256 + x[2] * 65536)
+
+class Processor
+{
+public:
+	std::map<std::string, int64> registers;
+	size_t pc = 0;
+	bool finished = false;
+
+	int64 value(const char* val)
+	{
+		if (isalpha(*val))
+		{
+			if (registers.count(val) == 0)
+			{
+				return 0;
+			}
+			else
+			{
+				return registers[val];
+			}
+		}
+		else
+		{
+			return (atoll(val));
+		}
+	}
+
+	int64 process( std::string input )
+	{
+		int at = 0;
+		finished = false;
+
+		while (!finished && pc >= 0 && pc < assembly.size())
+		{
+			std::string line = assembly[pc];
+			char* command = strtok(&*line.begin(), " ");
+			char* r = strtok(nullptr, " ");
+			std::string reg(r);
+			char* val = strtok(nullptr, " ");
+			if (registers.count(reg) == 0)
+			{
+				registers.insert(std::make_pair(reg, 0));
+				if (!isalpha(*r))
+				{
+					// Hack: if a number is used, store it as a register with its own value
+					registers[reg] = atoi(r);
+				}
+			}
+			switch (CASE(command))
+			{
+			case CASE("inp"): // e.g. "inp a"
+				registers[reg] = input[at++] - '0';
+				break;
+			case CASE("add"):
+				registers[reg] += value(val);
+				break;
+			case CASE("mul"):
+				registers[reg] *= value(val);
+				break;
+			case CASE("div"):
+				registers[reg] /= value(val);
+				break;
+			case CASE("mod"):
+				registers[reg] %= value(val);
+				break;
+			case CASE("eql"):
+				registers[reg] = (registers[reg] == value(val));
+				break;
+			default:
+				assert(false);
+				finished = true;
+				break;
+			}
+			if (!finished)
+			{
+				++pc;
+			}
+		}
+		return registers["z"];
+	}
+};
+
+void run(const char* filename, std::string input, int64 expectedZ, int part)
+{
+	printf("%s:\n", filename);
+	if (load(filename))
+	{
+		Processor p;
+		int64 z = p.process(input);
+		if (z == expectedZ)
+			printf("Part %i: %s -> %lld\n", part, input.c_str(), z);
+		else
+			printf("Part %i: %s doesn't work (z == %lld)\n", part, input.c_str(), z);
+	}
 }
 
 int main()
 {
-	Process("example.txt");
-	Process("input.txt");
+	run("example.txt", "9", 1, 0);
 
+	// See input_notes.txt for how the following numbers were worked out
+	run("input.txt", "74929995999389", 0, 1);
+	run("input.txt", "11118151637112", 0, 2);
+	
 	return 0;
 }
