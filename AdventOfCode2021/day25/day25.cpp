2e86b3a9566f8c4a111804ca9fc6d8bde5f07b46
Dave Knight <trave.uk@gmail.com>
2021-12-25 05:27:00 +0000

    AoC 2021 day 25 :)

--- AdventOfCode2021/day25/day25.cpp
+++ AdventOfCode2021/day25/day25.cpp
@@ -4,11 +4,94 @@
 
 #include "stdafx.h"
 
+enum Direction
+{
+	NONE,
+	EAST,
+	SOUTH
+};
+struct Cucumber
+{
+	Cucumber(coord _pos=coord(), Direction _dir=NONE)
+		: pos(_pos)
+		, dir(_dir)
+	{}
+	coord pos;
+	Direction dir;
+};
+
+std::map<coord, Cucumber> map;
+
+coord bottomRight;
+
+bool Step()
+{
+	bool bAnyMoved = false;
+
+	// Move east-facing cucumbers
+	std::vector<Cucumber> movedEast;
+	for (auto it : map)
+	{
+		Direction dir = it.second.dir;
+		if (dir != EAST)
+			continue;
+		coord pos = it.first;
+		coord nextPos = pos;
+		++nextPos.first;
+		if (nextPos.first > bottomRight.first)
+			nextPos.first = 0;
+		if (map.count(nextPos) == 0)
+		{
+			movedEast.push_back(it.second);
+			bAnyMoved = true;
+		}
+	}
+	for (Cucumber c: movedEast)
+	{
+		map.erase(c.pos);
+		++c.pos.first;
+		if (c.pos.first > bottomRight.first)
+			c.pos.first = 0;
+		map.insert_or_assign(c.pos, c);
+	}
+
+	// Move south-facing cucumbers
+	std::vector<Cucumber> movedSouth;
+	for (auto it : map)
+	{
+		Direction dir = it.second.dir;
+		if (dir != SOUTH)
+			continue;
+		coord pos = it.first;
+		coord nextPos = pos;
+		++nextPos.second;
+		if (nextPos.second > bottomRight.second)
+			nextPos.second = 0;
+		if (map.count(nextPos) == 0)
+		{
+			movedSouth.push_back(it.second);
+			bAnyMoved = true;
+		}
+	}
+	for (Cucumber c : movedSouth)
+	{
+		map.erase(c.pos);
+		++c.pos.second;
+		if (c.pos.second > bottomRight.second)
+			c.pos.second = 0;
+		map.insert_or_assign(c.pos, c);
+	}
+
+	return bAnyMoved;
+}
 void Process(const char* filename)
 {
-	int result = 0;
+	map.clear();
+	bottomRight = coord();
+
 	char* buffer = new char[65536];
 	FILE *fp = fopen(filename, "rt");
+	coord pos;
 	while (!feof(fp))
 	{
 		char* thisLine = fgets(buffer, 65536, fp);
@@ -17,14 +100,37 @@ void Process(const char* filename)
 			thisLine[strcspn(thisLine, "\n\r")] = '\0';
 			if (*thisLine)
 			{
-
+				for (pos.first = 0; pos.first < strlen(thisLine); ++pos.first)
+				{
+					char c = thisLine[pos.first];
+					if (pos.first > bottomRight.first)
+						bottomRight.first = pos.first;
+					if (pos.second > bottomRight.second)
+						bottomRight.second = pos.second;
+					
+					if (c == '.')
+					{
+						continue;
+					}
+					Direction dir = (c == '>') ? EAST : SOUTH;
+					Cucumber cum(pos, dir);
+					map.insert_or_assign(pos, cum);
+				}
+				++pos.second;
 			}
 		}
 	}
 	fclose(fp);
 	delete[] buffer;
 
-	printf("%s: Part 1: %d\n", filename, result);
+	int step = 0;
+	bool bAnyMoved = true;
+	while (bAnyMoved)
+	{
+		++step;
+		bAnyMoved = Step();
+	}
+	printf("%s: Part 1: %d\n", filename, step);
 }
 
 int main()
