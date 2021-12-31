86bec5ee5ba4e5577cebd267bb2861b4c66aac81
Dave Knight <trave.uk@gmail.com>
2021-12-31 20:08:44 +0000

    AoC 2015: day 4

--- AdventOfCode2015/day04/day04.cpp
+++ AdventOfCode2015/day04/day04.cpp
@@ -1,36 +1,38 @@
-// day04.cpp : Advent of Code 2021 https://adventofcode.com/2021
-// Solution by trave.uk 4/12/2021 https://github.com/trave-uk/AdventOfCode
+// day04.cpp : Advent of Code 2015 https://adventofcode.com/2015
+// Solution by trave.uk 31/12/2021 https://github.com/trave-uk/AdventOfCode
 //
 
 #include "stdafx.h"
 
-void Process(const char* filename)
+void Process(std::string input, int64 expectedPart1 = -1)
 {
-	int result = 0;
-	char* buffer = new char[65536];
-	FILE *fp = fopen(filename, "rt");
-	while (!feof(fp))
+	MD5 md5;
+	int64 part1 = -1;
+	int64 index = 0;
+	for (;;)
 	{
-		char* thisLine = fgets(buffer, 65536, fp);
-		if (thisLine)
+		std::string test = input + std::to_string(index);
+		char* digest = md5.digestString(test.c_str());
+		if (part1 == -1 && strncmp(digest, "00000", 5) == 0)
 		{
-			thisLine[strcspn(thisLine, "\n\r")] = '\0';
-			if (*thisLine)
-			{
-
-			}
+			part1 = index;
 		}
+		if (strncmp(digest, "000000", 6) == 0)
+		{
+			break;
+		}
+		++index;
 	}
-	fclose(fp);
-	delete[] buffer;
-
-	printf("%s: Part 1: %d\n", filename, result);
+	assert(expectedPart1 == -1 || expectedPart1 == part1);
+	printf("%s: Part 1: %lld\n", input.c_str(), part1);
+	printf("%s: Part 2: %lld\n", input.c_str(), index);
 }
 
 int main()
 {
-	Process("example.txt");
-	Process("input.txt");
+	Process("abcdef", 609043);
+	Process("pqrstuv", 1048970);
+	Process("ckczppom");
 
 	return 0;
 }
