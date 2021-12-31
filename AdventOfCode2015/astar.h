05b0577939ca9fe07afb33b32e55460fcb703ced
Dave Knight <trave.uk@gmail.com>
2021-12-31 15:57:28 +0000

    AoC 2015: updated common headers with latest versions from AoC 2021

--- AdventOfCode2015/astar.h
+++ AdventOfCode2015/astar.h
@@ -57,7 +57,11 @@ function A_Star(start, goal, h)
 
 #include <map>
 #include <queue>
-using int64 = long long;
+
+#ifndef _ASTAR_H
+#define _ASTAR_H
+
+using int64 = __int64;
 
 class coord : public std::pair<int64, int64>
 {
@@ -90,6 +94,10 @@ public:
 	int64 length()
 	{
 		return max(abs(first), abs(second));
+	}
+	bool isContained(const coord& min, const coord& max)
+	{
+		return first >= min.first && first <= max.first && second >= min.second && second <= max.second;
 	}
 };
 
@@ -127,14 +135,23 @@ template<class tNode> void print(coord& start, coord& goal, std::map<coord, tNod
 				_putch(' ');
 			else if (space[pos].isBlocked())
 				_putch('#');
-			else
+			else if (space[pos].getCost() == 1)
 				_putch('.');
+			else
+				_putch(space[pos].getCost() + '0');
 		}
 		_putch('\n');
 	}
 	_putch('\n');
 }
 #endif
+
+// optional class to derive from for the tNode class, implementing the required interface
+struct INode
+{
+	virtual bool isBlocked() const { return false; }
+	virtual int getCost() const { return 1; }
+};
 
 // class tNode must have an "isBlocked" function thus:
 //  bool tNode::isBlocked();
@@ -204,7 +221,7 @@ template<class tNode> int64 aStarSearch(coord& start, coord& goal, std::map<coor
 				continue;
 
 			// tentative_gScore is the distance from start to the neighbour through current
-			int64 tentative_gScore = gScore[current] + 1;
+			int64 tentative_gScore = gScore[current] + space[neighbour].getCost();
 			int64 neighbour_gScore = (gScore.count(neighbour) ? gScore[neighbour] : INT_MAX);
 			if (tentative_gScore < neighbour_gScore)
 			{
@@ -218,3 +235,5 @@ template<class tNode> int64 aStarSearch(coord& start, coord& goal, std::map<coor
 	}
 	return -1; // failed to find a path
 }
+
+#endif // _ASTAR_H
--- AdventOfCode2015/libheaders.h
+++ AdventOfCode2015/libheaders.h
@@ -23,13 +23,18 @@
 #include <windows.h>
 #include <processenv.h>
 #include <winnt.h>
-#include <wincon.h>
-#include <conio.h>
 
 #define _ARRAYSIZE_(x) (sizeof(x)/sizeof(*x))
 
 #include "crc.h"
 #include "md5.h"
 #include "astar.h"
+#include "term.h"
 
 using int64 = __int64;
+
+// From https://stackoverflow.com/questions/1903954/is-there-a-standard-sign-function-signum-sgn-in-c-c
+template <typename T> int sgn(T val)
+{
+	return (T(0) < val) - (val < T(0));
+}
