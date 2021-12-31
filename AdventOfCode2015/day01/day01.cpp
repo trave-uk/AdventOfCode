da0f584d0c3643a87c23bf9f5f9bb473926e5b1b
Dave Knight <trave.uk@gmail.com>
2021-12-31 20:07:56 +0000

    AoC 2015: day 1

--- AdventOfCode2015/day01/day01.cpp
+++ AdventOfCode2015/day01/day01.cpp
@@ -1,5 +1,5 @@
-// day01.cpp : Advent of Code 2021 https://adventofcode.com/2021
-// Solution by trave.uk 1/12/2021 https://github.com/trave-uk/AdventOfCode
+// day01.cpp : Advent of Code 2015 https://adventofcode.com/2015
+// Solution by trave.uk 31/12/2021 https://github.com/trave-uk/AdventOfCode
 //
 
 #include "stdafx.h"
@@ -9,6 +9,7 @@ void Process(const char* filename)
 	int result = 0;
 	char* buffer = new char[65536];
 	FILE *fp = fopen(filename, "rt");
+	int part2 = -1;
 	while (!feof(fp))
 	{
 		char* thisLine = fgets(buffer, 65536, fp);
@@ -17,14 +18,37 @@ void Process(const char* filename)
 			thisLine[strcspn(thisLine, "\n\r")] = '\0';
 			if (*thisLine)
 			{
-
+				int pos = 0;
+				std::string line(thisLine);
+				int index = 0;
+				for (char c : line)
+				{
+					switch (c)
+					{
+					case '(':
+						++pos;
+						break;
+					case ')':
+						--pos;
+						if (pos == -1 && part2 == -1)
+						{
+							part2 = index + 1;
+						}
+						break;
+					default:
+						assert(false);
+						break;
+					}
+					++index;
+				}
+				printf("%s: Part 1: %d\n", filename, pos);
+				printf("%s: Part 2: %d\n", filename, part2);
 			}
 		}
 	}
 	fclose(fp);
 	delete[] buffer;
 
-	printf("%s: Part 1: %d\n", filename, result);
 }
 
 int main()
