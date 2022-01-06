3fbdf5bf4106318f57b8c849bf767b1f9ba3d717
Dave Knight <trave.uk@gmail.com>
2022-01-06 18:03:27 +0000

    AoC 2015: day 10

--- AdventOfCode2015/day10/day10.cpp
+++ AdventOfCode2015/day10/day10.cpp
@@ -1,41 +1,52 @@
 // day10.cpp : Advent of Code 2015 https://adventofcode.com/2015
-// Solution by trave.uk 10/12/2021 https://github.com/trave-uk/AdventOfCode
+// Solution by trave.uk 6/1/2022 https://github.com/trave-uk/AdventOfCode
 //
 
 #include "stdafx.h"
 
-void Process(const char* filename, int64 expectedPart1 = -1, int64 expectedPart2 = -1)
+void Process(std::string input, int part, int times, int64 expectedPart1 = -1)
 {
-	int64 part1 = 0;
-	int64 part2 = 0;
-	char* buffer = new char[65536];
-	FILE *fp = fopen(filename, "rt");
-	while (!feof(fp))
+	std::string result = input;
+	for (int i = 0; i < times; ++i)
 	{
-		char* thisLine = fgets(buffer, 65536, fp);
-		if (thisLine)
+		std::string next;
+		char digit = 0;
+		int count = 0;
+		for (char c : result)
 		{
-			thisLine[strcspn(thisLine, "\n\r")] = '\0';
-			if (*thisLine)
+			if (c == digit)
 			{
-
+				++count;
 			}
+			else if (digit)
+			{
+				char add[20];
+				sprintf(add, "%d%c", count, digit);
+				next.append(add);
+				count = 1;
+			}
+			else
+			{
+				count = 1;
+			}
+			digit = c;
 		}
+		char add[20];
+		sprintf(add, "%d%c", count, digit);
+		next.append(add);
+		result = next;
 	}
-	fclose(fp);
-	delete[] buffer;
 
+	int64 part1 = result.length();
 	assert(expectedPart1 == -1 || expectedPart1 == part1);
-	printf("%s: Part 1: %lld\n", filename, part1);
-
-//	assert(expectedPart2 == -1 || expectedPart2 == part2);
-//	printf("%s: Part 2: %lld\n", filename, part2);
+	printf("%s: Part %d: %lld\n", input.c_str(), part, part1);
 }
 
 int main()
 {
-	Process("example.txt");
-	Process("input.txt");
+	Process("1", 1, 5, 6);
+	Process("1113122113", 1, 40);
+	Process("1113122113", 2, 50);
 
 	return 0;
 }
