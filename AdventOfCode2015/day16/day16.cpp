77c6e6676b9a36f2383a79ff0f2d04b5a253814f
Dave Knight <trave.uk@gmail.com>
2022-03-13 15:36:49 +0000

    AoC 2015: day 16

--- AdventOfCode2015/day16/day16.cpp
+++ AdventOfCode2015/day16/day16.cpp
@@ -1,13 +1,120 @@
 // day16.cpp : Advent of Code 2015 https://adventofcode.com/2015
-// Solution by trave.uk 16/12/2021 https://github.com/trave-uk/AdventOfCode
+// Solution by trave.uk 13/03/2022 https://github.com/trave-uk/AdventOfCode
 //
 
 #include "stdafx.h"
 
-void Process(const char* filename, int64 expectedPart1 = -1, int64 expectedPart2 = -1)
+struct Sue
 {
-	int64 part1 = 0;
-	int64 part2 = 0;
+	Sue() = default;
+	Sue(char* input)
+	{
+		// Sue 1: goldfish: 9, cars: 0, samoyeds: 9
+
+		char* tok = strtok(input, ":");
+		int found = sscanf(tok, "Sue %d", &index);
+		assert(found == 1);
+		bool bPossiblePart1 = true;
+		bool bPossiblePart2 = true;
+		while (tok = strtok(nullptr, ","))
+		{
+			char nam[20];
+			int amount;
+			found = sscanf(tok, " %[^:]: %d", nam, &amount);
+			assert(found == 2);
+			std::string name = nam;
+			if (name == "children")
+			{
+				children = amount;
+				if (children != 3)
+					bPossiblePart1 = bPossiblePart2 = false;
+			}
+			else if (name == "cats")
+			{
+				cats = amount;
+				if (cats != 7)
+					bPossiblePart1 = false;
+				if (cats <= 7)
+					bPossiblePart2 = false;
+			}
+			else if (name == "samoyeds")
+			{
+				samoyeds = amount;
+				if (samoyeds != 2)
+					bPossiblePart1 = bPossiblePart2 = false;
+			}
+			else if (name == "pomeranians")
+			{
+				pomeranians = amount;
+				if (pomeranians != 3)
+					bPossiblePart1 = false;
+				if (pomeranians >= 3)
+					bPossiblePart2 = false;
+			}
+			else if (name == "akitas")
+			{
+				akitas = amount;
+				if (akitas != 0)
+					bPossiblePart1 = bPossiblePart2 = false;
+			}
+			else if (name == "vizslas")
+			{
+				vizslas = amount;
+				if (vizslas != 0)
+					bPossiblePart1 = bPossiblePart2 = false;
+			}
+			else if (name == "goldfish")
+			{
+				goldfish = amount;
+				if (goldfish != 5)
+					bPossiblePart1 = false;
+				if (goldfish >= 5)
+					bPossiblePart2 = false;
+			}
+			else if (name == "trees")
+			{
+				trees = amount;
+				if (trees != 3)
+					bPossiblePart1 = false;
+				if (trees <= 3)
+					bPossiblePart2 = false;
+			}
+			else if (name == "cars")
+			{
+				cars = amount;
+				if (cars != 2)
+					bPossiblePart1 = bPossiblePart2 = false;
+			}
+			else if (name == "perfumes")
+			{
+				perfumes = amount;
+				if (perfumes != 1)
+					bPossiblePart1 = bPossiblePart2 = false;
+			}
+			else
+				assert(false);
+		}
+		if (bPossiblePart1)
+			printf("Part 1: Sue %d matches\n", index);
+		if (bPossiblePart2)
+			printf("Part 2: Sue %d matches\n", index);
+	}
+	int index;
+
+	int children = -1;
+	int cats = -1;
+	int samoyeds = -1;
+	int pomeranians = -1;
+	int akitas = -1;
+	int vizslas = -1;
+	int goldfish = -1;
+	int trees = -1;
+	int cars = -1;
+	int perfumes = -1;
+};
+
+void Process(const char* filename)
+{
 	char* buffer = new char[65536];
 	FILE *fp = fopen(filename, "rt");
 	while (!feof(fp))
@@ -18,23 +125,29 @@ void Process(const char* filename, int64 expectedPart1 = -1, int64 expectedPart2
 			thisLine[strcspn(thisLine, "\n\r")] = '\0';
 			if (*thisLine)
 			{
-
+				// Sue 1: goldfish: 9, cars: 0, samoyeds: 9
+				/*
+				children: 3
+				cats: 7
+				samoyeds: 2
+				pomeranians: 3
+				akitas: 0
+				vizslas: 0
+				goldfish: 5
+				trees: 3
+				cars: 2
+				perfumes: 1
+				*/
+				Sue s(thisLine);
 			}
 		}
 	}
 	fclose(fp);
 	delete[] buffer;
-
-	assert(expectedPart1 == -1 || expectedPart1 == part1);
-	printf("%s: Part 1: %lld\n", filename, part1);
-
-//	assert(expectedPart2 == -1 || expectedPart2 == part2);
-//	printf("%s: Part 2: %lld\n", filename, part2);
 }
 
 int main()
 {
-	Process("example.txt");
 	Process("input.txt");
 
 	return 0;
