04338aaeab7af7d99d742e8e556d86d643107b0d
Dave Knight <trave.uk@gmail.com>
2022-12-04 22:36:57 +0000

    AoC 2022 day 4

--- AdventOfCode2022/day04/day04.cpp
+++ AdventOfCode2022/day04/day04.cpp
@@ -4,6 +4,20 @@
 
 #include "stdafx.h"
 
+struct Range
+{
+	int start;
+	int end;
+	bool contains(const Range& other)
+	{
+		return start <= other.start && end >= other.end;
+	}
+	bool contains(const int other)
+	{
+		return start <= other && end >= other;
+	}
+};
+
 void Process(const char* filename, int64 expectedPart1 = -1, int64 expectedPart2 = -1)
 {
 	int64 part1 = 0;
@@ -18,7 +32,13 @@ void Process(const char* filename, int64 expectedPart1 = -1, int64 expectedPart2
 			thisLine[strcspn(thisLine, "\n\r")] = '\0';
 			if (*thisLine)
 			{
-
+				Range r1, r2;
+				int m = sscanf(thisLine, "%d-%d,%d-%d", &r1.start, &r1.end, &r2.start, &r2.end);
+				assert(m == 4);
+				if (r1.contains(r2) || r2.contains(r1))
+					++part1;
+				if (r1.contains(r2.start) || r1.contains(r2.end) || r2.contains(r1.start) || r2.contains(r1.end))
+					++part2;
 			}
 		}
 	}
@@ -28,13 +48,13 @@ void Process(const char* filename, int64 expectedPart1 = -1, int64 expectedPart2
 	assert(expectedPart1 == -1 || expectedPart1 == part1);
 	printf("%s: Part 1: %lld\n", filename, part1);
 
-//	assert(expectedPart2 == -1 || expectedPart2 == part2);
-//	printf("%s: Part 2: %lld\n", filename, part2);
+	assert(expectedPart2 == -1 || expectedPart2 == part2);
+	printf("%s: Part 2: %lld\n", filename, part2);
 }
 
 int main()
 {
-	Process("example.txt");
+	Process("example.txt", 2, 4);
 	Process("input.txt");
 
 	return 0;
