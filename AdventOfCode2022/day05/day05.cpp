5916dfba11e6b91bb4b0eb01d67a69bf95c4deed
Dave Knight <trave.uk@gmail.com>
2022-12-05 05:36:25 +0000

    AoC 2022 day 5

--- AdventOfCode2022/day05/day05.cpp
+++ AdventOfCode2022/day05/day05.cpp
@@ -4,12 +4,23 @@
 
 #include "stdafx.h"
 
-void Process(const char* filename, int64 expectedPart1 = -1, int64 expectedPart2 = -1)
+using Stack = std::stack<char>;
+struct Move
 {
-	int64 part1 = 0;
-	int64 part2 = 0;
+	int number;
+	int from;
+	int to;
+};
+
+void Process(const char* filename, int part = -1, std::string expectedResult = "")
+{
+	std::vector<Stack> stacks;
+	std::vector<Move> moves;
+
+	std::string result = "";
 	char* buffer = new char[65536];
 	FILE *fp = fopen(filename, "rt");
+
 	while (!feof(fp))
 	{
 		char* thisLine = fgets(buffer, 65536, fp);
@@ -18,24 +29,97 @@ void Process(const char* filename, int64 expectedPart1 = -1, int64 expectedPart2
 			thisLine[strcspn(thisLine, "\n\r")] = '\0';
 			if (*thisLine)
 			{
+				if (*thisLine == ' ' || *thisLine == '[')
+				{
+					// stacks
+					if (isdigit(thisLine[1]))
+					{
+						continue;
+					}
+					// e.g. "[Z] [M] [P]"
+					for (int i = 1, s = 0; i < strlen(thisLine); i += 4, ++s)
+					{
+						char crate = thisLine[i];
+						while (s+1 > stacks.size())
+						{
+							stacks.push_back(Stack());
+						}
+						if (isalpha(crate))
+						{
+							stacks[s].push(crate);
+						}
+					}
+				}
+				else if (*thisLine == 'm')
+				{
+					// moves
+					// e.g. "move 3 from 1 to 3"
+					Move move;
+					int m = sscanf(thisLine, "move %d from %d to %d", &move.number, &move.from, &move.to);
+					assert(m == 3);
 
+					moves.push_back(move);
+				}
 			}
 		}
+	}
+	// all the stacks are upside down (read in top-to-bottom) so invert them.
+	for (int i = 0; i < stacks.size(); ++i)
+	{
+		Stack copy;
+		while (!stacks[i].empty())
+		{
+			copy.push(stacks[i].top());
+			stacks[i].pop();
+		}
+		stacks[i] = copy;
 	}
 	fclose(fp);
 	delete[] buffer;
 
-	assert(expectedPart1 == -1 || expectedPart1 == part1);
-	printf("%s: Part 1: %lld\n", filename, part1);
+	for (Move m : moves)
+	{
+		Stack temp;
+		for (int i = 0; i < m.number; ++i)
+		{
+			assert(!stacks[m.from - 1].empty());
+			if (part == 1)
+			{
+				stacks[m.to - 1].push(stacks[m.from - 1].top());
+			}
+			else
+			{
+				assert(part == 2);
+				temp.push(stacks[m.from - 1].top());
+			}
+			stacks[m.from - 1].pop();
+		}
+		if (part == 2)
+		{
+			for (int i = 0; i < m.number; ++i)
+			{
+				assert(!temp.empty());
+				stacks[m.to - 1].push(temp.top());
+				temp.pop();
+			}
+		}
+	}
 
-//	assert(expectedPart2 == -1 || expectedPart2 == part2);
-//	printf("%s: Part 2: %lld\n", filename, part2);
+	for (Stack s : stacks)
+	{
+		result.append(1, s.top());
+	}
+
+	assert(expectedResult == "" || expectedResult == result);
+	printf("%s: Part %d: %s\n", filename, part, result.c_str());
 }
 
 int main()
 {
-	Process("example.txt");
-	Process("input.txt");
+	Process("example.txt", 1, "CMZ");
+	Process("example.txt", 2, "MCD");
+	Process("input.txt", 1);
+	Process("input.txt", 2);
 
 	return 0;
 }
