5ef71514ebdd0e805405d12f9bb8af6c454734be
Dave Knight <trave.uk@gmail.com>
2022-12-09 05:59:13 +0000

    AoC 2022 day 9

--- AdventOfCode2022/day09/day09.cpp
+++ AdventOfCode2022/day09/day09.cpp
@@ -4,10 +4,31 @@
 
 #include "stdafx.h"
 
-void Process(const char* filename, int64 expectedPart1 = -1, int64 expectedPart2 = -1)
+using Coord = std::pair<int64, int64>;
+
+using Cloud = std::set<Coord>;
+
+void Catch(Coord& T, const Coord& H)
 {
-	int64 part1 = 0;
-	int64 part2 = 0;
+	int move1 = H.first - T.first;
+	int move2 = H.second - T.second;
+	if (abs(move1) > 1 || abs(move2) > 1)
+	{
+		T.first += sgn(move1);
+		T.second += sgn(move2);
+	}
+}
+
+void Process(const char* filename, int part, int64 expectedResult = -1)
+{
+	int RopeLength = (part == 1) ? 2 : 10;
+	std::vector<Coord> Rope;
+	Rope.assign(RopeLength, Coord());
+	Coord& H = Rope[0];
+	Coord& T = Rope[RopeLength - 1];
+	Cloud Been;
+	Been.insert(Rope[0]);
+	int64 result = 0;
 	char* buffer = new char[65536];
 	FILE *fp = fopen(filename, "rt");
 	while (!feof(fp))
@@ -18,24 +39,51 @@ void Process(const char* filename, int64 expectedPart1 = -1, int64 expectedPart2
 			thisLine[strcspn(thisLine, "\n\r")] = '\0';
 			if (*thisLine)
 			{
-
+				char dir;
+				int dist;
+				int m = sscanf(thisLine, "%c %d", &dir, &dist);
+				 
+				for (int i = 0; i < dist; ++i)
+				{
+					switch (dir)
+					{
+					case 'R':
+						++H.first;
+						break;
+					case 'L':
+						--H.first;
+						break;
+					case 'U':
+						--H.second;
+						break;
+					case 'D':
+						++H.second;
+						break;
+					}
+					for (int j = 0; j < RopeLength - 1; ++j)
+					{
+						Catch(Rope[j+1], Rope[j]);
+					}
+					Been.insert(T);
+				}
 			}
 		}
 	}
 	fclose(fp);
 	delete[] buffer;
 
-	assert(expectedPart1 == -1 || expectedPart1 == part1);
-	printf("%s: Part 1: %lld\n", filename, part1);
-
-//	assert(expectedPart2 == -1 || expectedPart2 == part2);
-//	printf("%s: Part 2: %lld\n", filename, part2);
+	result = Been.size();
+	assert(expectedResult == -1 || expectedResult == result);
+	printf("%s: Part %d: %lld\n", filename, part, result);
 }
 
 int main()
 {
-	Process("example.txt");
-	Process("input.txt");
+	Process("example1.txt", 1, 13);
+	Process("input.txt", 1);
+
+	Process("example2.txt", 2, 36);
+	Process("input.txt", 2);
 
 	return 0;
 }
