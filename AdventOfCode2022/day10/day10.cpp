40270283cb734fa770be7ee833a5fd8af9b7e723
Dave Knight <trave.uk@gmail.com>
2022-12-10 13:26:51 +0000

    AoC 2022 day 10

--- AdventOfCode2022/day10/day10.cpp
+++ AdventOfCode2022/day10/day10.cpp
@@ -4,12 +4,44 @@
 
 #include "stdafx.h"
 
-void Process(const char* filename, int64 expectedPart1 = -1, int64 expectedPart2 = -1)
+void TestPart1(int64& part1, int64 X, int cycle)
+{
+	if (cycle == 20 || cycle == 60 || cycle == 100 || cycle == 140 || cycle == 180 || cycle == 220)
+	{
+		part1 += cycle * X;
+	}
+}
+
+void TestPart2(std::array<std::array<char, 41>, 6>& part2, int64 X, int cycle)
+{
+	int x = (cycle - 1) % 40;
+	int y = (cycle - 1) / 40;
+	if (abs(x - X) <= 1)
+	{
+		part2[y][x] = '#';
+	}
+	else
+	{
+		part2[y][x] = '.';
+	}
+}
+
+void Process(const char* filename, int64 expectedPart1 = -1)
 {
 	int64 part1 = 0;
-	int64 part2 = 0;
+
+	std::array<std::array<char, 41>, 6> part2;
+	memset(&part2[0][0], ' ', 41 * 6);
+	for (auto& row : part2)
+	{
+		row[40] = '\0';
+	}
+
 	char* buffer = new char[65536];
 	FILE *fp = fopen(filename, "rt");
+	int cycle = 0;
+	int64 X = 1;
+
 	while (!feof(fp))
 	{
 		char* thisLine = fgets(buffer, 65536, fp);
@@ -18,7 +50,32 @@ void Process(const char* filename, int64 expectedPart1 = -1, int64 expectedPart2
 			thisLine[strcspn(thisLine, "\n\r")] = '\0';
 			if (*thisLine)
 			{
-
+				char op[80];
+				int value;
+				int matches = sscanf(thisLine, "%s %d", op, &value);
+				std::string Op(op);
+				if (Op == "noop")
+				{
+					assert(matches == 1);
+					++cycle;
+					TestPart1(part1, X, cycle);
+					TestPart2(part2, X, cycle);
+				}
+				else if (Op == "addx")
+				{
+					assert(matches == 2);
+					++cycle;
+					TestPart1(part1, X, cycle);
+					TestPart2(part2, X, cycle);
+					++cycle;
+					TestPart1(part1, X, cycle);
+					TestPart2(part2, X, cycle);
+					X += value;
+				}
+				else
+				{
+					assert(false);
+				}
 			}
 		}
 	}
@@ -28,13 +85,16 @@ void Process(const char* filename, int64 expectedPart1 = -1, int64 expectedPart2
 	assert(expectedPart1 == -1 || expectedPart1 == part1);
 	printf("%s: Part 1: %lld\n", filename, part1);
 
-//	assert(expectedPart2 == -1 || expectedPart2 == part2);
-//	printf("%s: Part 2: %lld\n", filename, part2);
+	printf("%s: Part 2:\n", filename);
+	for (auto row : part2)
+	{
+		printf("%s\n", &row.front());
+	}
 }
 
 int main()
 {
-	Process("example.txt");
+	Process("example.txt", 13140);
 	Process("input.txt");
 
 	return 0;
