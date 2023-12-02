e6ce6e48850c49f84e5a338336b288350addafea
Dave Knight <trave.uk@gmail.com>
2023-12-02 05:25:16 +0000

    AoC 2023 day 2

--- AdventOfCode2023/day02/day02.cpp
+++ AdventOfCode2023/day02/day02.cpp
@@ -4,6 +4,8 @@
 
 #include "stdafx.h"
 
+// only 12 red cubes, 13 green cubes, and 14 blue cubes
+
 void Process(const char* filename, int64 expectedPart1 = -1, int64 expectedPart2 = -1)
 {
 	char* buffer = new char[65536];
@@ -18,8 +20,54 @@ void Process(const char* filename, int64 expectedPart1 = -1, int64 expectedPart2
 			thisLine[strcspn(thisLine, "\n\r")] = '\0';
 			if (*thisLine)
 			{
-				int pos = 0;
-				std::string line(thisLine);
+				char* context;
+				char* tok = strtok_s(thisLine, ":", &context);
+				int64 gameid = 0;
+				int result = sscanf(tok, "Game %lld", &gameid);
+				assert(result == 1);
+				tok = strtok_s(nullptr, ";", &context);
+				bool possible = true;
+				int minred = 0;
+				int minblue = 0;
+				int mingreen = 0;
+				while (tok)
+				{
+					int red = 0, green = 0, blue = 0;
+					char* context2;
+					char* tok2 = strtok_s(tok, ",", &context2);
+					while (tok2)
+					{
+						int amount;
+						char col[10];
+						result = sscanf(tok2, "%d %s", &amount, col);
+						assert(result == 2);
+
+						std::string colour(col);
+						if (colour == "red")
+							red = amount;
+						else if (colour == "green")
+							green = amount;
+						else if (colour == "blue")
+							blue = amount;
+						tok2 = strtok_s(nullptr, ",", &context2);
+					}
+
+					if (red > 12 || green > 13 || blue > 14)
+					{
+						possible = false;
+					}
+
+					if (red > minred)
+						minred = red;
+					if (blue > minblue)
+						minblue = blue;
+					if (green > mingreen)
+						mingreen = green;
+					tok = strtok_s(nullptr, ";", &context);
+				}
+				if (possible)
+					part1 += gameid;
+				part2 += minred * minblue * mingreen;
 
 			}
 		}
@@ -37,7 +85,7 @@ void Process(const char* filename, int64 expectedPart1 = -1, int64 expectedPart2
 
 int main()
 {
-	Process("example.txt");
+	Process("example.txt", 8, 2286);
 	Process("input.txt");
 
 	return 0;
