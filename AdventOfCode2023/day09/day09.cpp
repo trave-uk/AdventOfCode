063568bfdd3e573f7d3295c3c3c373acc3a18365
Dave Knight <trave.uk@gmail.com>
2023-12-09 05:46:57 +0000

    AoC 2023 day 9

--- AdventOfCode2023/day09/day09.cpp
+++ AdventOfCode2023/day09/day09.cpp
@@ -4,6 +4,41 @@
 
 #include "stdafx.h"
 
+void ReadInts(char* thisLine, std::vector<int64>& output)
+{
+	char* tok = strtok(thisLine, " ");
+	while (tok)
+	{
+		output.push_back(atoll(tok));
+		tok = strtok(nullptr, " ");
+	}
+}
+
+std::pair<int64, int64> Predict(std::vector<int64>& history)
+{
+	std::vector<int64> diffs;
+	int64 length = history.size();
+	bool allZero = true;
+	for (int64 i = 1; i < length; ++i)
+	{
+		int64 diff = history[i] - history[i - 1];
+		diffs.push_back(diff);
+		if (diff)
+			allZero = false;
+	}
+	int64 newDiffFront = 0;
+	int64 newDiffBack = 0;
+	if (!allZero)
+	{
+		auto x = Predict(diffs);
+		newDiffFront = x.first;
+		newDiffBack = x.second;
+	}
+	int64 first = history.front() - newDiffFront;
+	int64 last = history.back() + newDiffBack;
+	return std::make_pair(first, last);
+}
+
 void Process(const char* filename, int64 expectedPart1 = -1, int64 expectedPart2 = -1)
 {
 	char* buffer = new char[65536];
@@ -18,9 +53,11 @@ void Process(const char* filename, int64 expectedPart1 = -1, int64 expectedPart2
 			thisLine[strcspn(thisLine, "\n\r")] = '\0';
 			if (*thisLine)
 			{
-				int pos = 0;
-				std::string line(thisLine);
-
+				std::vector<int64> history;
+				ReadInts(thisLine, history);
+				auto x = Predict(history);
+				part1 += x.second;
+				part2 += x.first;
 			}
 		}
 	}
@@ -37,7 +74,7 @@ void Process(const char* filename, int64 expectedPart1 = -1, int64 expectedPart2
 
 int main()
 {
-	Process("example.txt");
+	Process("example.txt", 114, 2);
 	Process("input.txt");
 
 	return 0;
