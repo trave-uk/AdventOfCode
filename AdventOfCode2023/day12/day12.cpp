9f5a885663369845415a8f2dab3f2b253ff35c0c
Dave Knight <trave.uk@gmail.com>
2023-12-13 11:27:31 +0000

    AoC 2023 day 12: part 2 brute force far too slow

--- AdventOfCode2023/day12/day12.cpp
+++ AdventOfCode2023/day12/day12.cpp
@@ -4,6 +4,107 @@
 
 #include "stdafx.h"
 
+using Gaps = std::vector<int64>;
+
+std::string g_pattern;
+std::vector<int64> g_numbers;
+
+bool MatchPartialPattern(const Gaps& gaps)
+{
+	auto p_iter = g_pattern.begin();
+	auto n_iter = g_numbers.begin();
+	for (int64 gap : gaps)
+	{
+		while (gap)
+		{
+			char ch = *p_iter++;
+			if (ch != '.' && ch != '?')
+				return false;
+			--gap;
+		}
+		if (n_iter != g_numbers.end())
+		{
+			int64 block = *n_iter++;
+			while (block)
+			{
+				char ch = *p_iter++;
+				if (ch != '#' && ch != '?')
+					return false;
+				--block;
+			}
+			if (n_iter != g_numbers.end())
+			{
+				char ch = *p_iter++;
+				if (ch != '.' && ch != '?')
+					return false;
+			}
+		}
+	}
+	// count the remaining blocks in the pattern to see whether they could match the remaining numbers
+	int64 blocks = 0, unknown = 0;
+	while (p_iter != g_pattern.end())
+	{
+		char ch = *p_iter++;
+		if (ch == '#') ++blocks;
+		else if (ch == '?') ++unknown;
+	}
+	int64 expecting = 0;
+	while (n_iter != g_numbers.end())
+	{
+		expecting += *n_iter++;
+	}
+	if (expecting > (blocks + unknown))
+		return false;
+	if (expecting < blocks)
+		return false;
+	return true;
+}
+
+void CountPatterns(int64& numPatterns, Gaps g, int64 extraSpaces, int64 gaps)
+{
+	if (extraSpaces == 0)
+	{
+		while (gaps)
+		{
+			g.push_back(0);
+			--gaps;
+		}
+		if (MatchPartialPattern(g))
+			++numPatterns;
+		return;
+	}
+	if (gaps == 1)
+	{
+		g.push_back(extraSpaces);
+		if (MatchPartialPattern(g))
+			++numPatterns;
+		return;
+	}
+	if (!MatchPartialPattern(g))
+		return;
+	for (int64 thisGap = 0; thisGap <= extraSpaces; ++thisGap)
+	{
+		Gaps g2 = g;
+		g2.push_back(thisGap);
+		CountPatterns(numPatterns, g2, extraSpaces - thisGap, gaps - 1);
+	}
+}
+
+int64 CalculateScore(const std::string& pattern, const std::vector<int64>& numbers, int64 size)
+{
+	// find all patterns that match the numbers
+	int64 extraSpaces = pattern.length() - size; // the number of extra spaces that could be between banks of '#'
+	int64 gaps = numbers.size() + 1; // the number of places that extra spaces can be placed: either side of or between the banks of '#'.
+
+	// so, for a list of ints size (gaps), find all the ways that the extra spaces can be distributed.
+	int64 numPatterns = 0;
+	g_pattern = pattern;
+	g_numbers = numbers;
+	CountPatterns(numPatterns, Gaps(), extraSpaces, gaps);
+
+	return numPatterns;
+}
+
 void Process(const char* filename, int64 expectedPart1 = -1, int64 expectedPart2 = -1)
 {
 	char* buffer = new char[65536];
@@ -18,9 +119,37 @@ void Process(const char* filename, int64 expectedPart1 = -1, int64 expectedPart2
 			thisLine[strcspn(thisLine, "\n\r")] = '\0';
 			if (*thisLine)
 			{
-				int pos = 0;
-				std::string line(thisLine);
+				// ????.######..#####. 1,6,5
+				char* p = strtok(thisLine, " ");
+				std::string pattern(p);
+				std::vector<int64> numbers;
+				char* number = strtok(nullptr, ",");
+				int64 size = -1;
+				while (number)
+				{
+					int64 num = atoll(number);
+					numbers.push_back(num);
+					number = strtok(nullptr, ",");
+					size += num + 1;
+				}
+				int64 part1score = CalculateScore(pattern, numbers, size);
+				part1 += part1score;
 
+				// for part 2, initially brute force it
+				std::string pattern2;
+				std::vector<int64> numbers2;
+				for (int i = 0; i < 5; ++i)
+				{
+					pattern2.append(pattern);
+					if (i < 4)
+						pattern2.append(1, '?');
+					numbers2.insert(numbers2.end(), numbers.begin(), numbers.end());
+				}
+				int64 size2 = size * 5 + 4;;
+				
+				int64 part2score = CalculateScore(pattern2, numbers2, size2);
+				part2 += part2score;
+				printf("1: %lld 2: %lld\n", part1score, part2score);
 			}
 		}
 	}
@@ -37,7 +166,7 @@ void Process(const char* filename, int64 expectedPart1 = -1, int64 expectedPart2
 
 int main()
 {
-	Process("example.txt");
+	Process("example.txt", 21, 525152);
 	Process("input.txt");
 
 	return 0;
