e5c3dccacc6667de51c6a790801796107cd3949f
Dave Knight <trave.uk@gmail.com>
2024-11-26 20:45:50 +0000

    Added 2024 solution and stub projects.

---
+++ AdventOfCode2024/day21/day21.cpp
@@ -0,0 +1,44 @@
+// day21.cpp : Advent of Code 2024 https://adventofcode.com/2024
+// Solution by trave.uk 21/12/2024 https://github.com/trave-uk/AdventOfCode
+//
+
+#include "stdafx.h"
+
+void Process(const char* filename, int64 expectedPart1 = -1, int64 expectedPart2 = -1)
+{
+	char* buffer = new char[65536];
+	FILE* fp = fopen(filename, "rt");
+	int64 part1 = 0;
+	int64 part2 = 0;
+	while (!feof(fp))
+	{
+		char* thisLine = fgets(buffer, 65536, fp);
+		if (thisLine)
+		{
+			thisLine[strcspn(thisLine, "\n\r")] = '\0';
+			if (*thisLine)
+			{
+				int pos = 0;
+				std::string line(thisLine);
+
+			}
+		}
+	}
+	fclose(fp);
+	delete[] buffer;
+
+	assert(expectedPart1 == -1 || expectedPart1 == part1);
+	printf("%s: Part 1: %lld\n", filename, part1);
+
+	assert(expectedPart2 == -1 || expectedPart2 == part2);
+	printf("%s: Part 2: %lld\n", filename, part2);
+
+}
+
+int main()
+{
+	Process("example.txt");
+	Process("input.txt");
+
+	return 0;
+}
