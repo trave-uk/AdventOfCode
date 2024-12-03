78b25e46b9fb63b1d35400f6459a434d618b94f8
Dave Knight <trave.uk@gmail.com>
2024-12-03 14:40:48 +0000

    AoC 2024 day 2

--- AdventOfCode2024/day02/day02.cpp
+++ AdventOfCode2024/day02/day02.cpp
@@ -1,8 +1,45 @@
 // day02.cpp : Advent of Code 2024 https://adventofcode.com/2024
-// Solution by trave.uk 02/12/2024 https://github.com/trave-uk/AdventOfCode
+// Solution by trave.uk 03/12/2024 https://github.com/trave-uk/AdventOfCode
 //
 
 #include "stdafx.h"
+
+bool safe(std::vector<int> numbers)
+{
+	int last = INT_MAX;
+	int lastdiff = INT_MAX;
+	for (int n : numbers)
+	{
+		if (last == INT_MAX)
+		{
+			last = n;
+			continue;
+		}
+		int diff = n - last;
+		if (diff == 0 || abs(diff) > 3)
+			return false;
+		if (lastdiff != INT_MAX && sgn(diff) != sgn(lastdiff))
+			return false;
+		lastdiff = diff;
+		last = n;
+	}
+	return true;
+}
+
+bool safe2(std::vector<int> numbers)
+{
+	if (safe(numbers))
+		return true;
+	for (int i = 0; i < numbers.size(); ++i)
+	{
+		std::vector<int> numbers2(numbers.begin(), numbers.begin() + i);
+		std::vector<int> numbers3(numbers.begin() + i + 1, numbers.end());
+		numbers2.insert(numbers2.end(), numbers3.begin(), numbers3.end());
+		if (safe(numbers2))
+			return true;
+	}
+	return false;
+}
 
 void Process(const char* filename, int64 expectedPart1 = -1, int64 expectedPart2 = -1)
 {
@@ -19,8 +56,17 @@ void Process(const char* filename, int64 expectedPart1 = -1, int64 expectedPart2
 			if (*thisLine)
 			{
 				int pos = 0;
-				std::string line(thisLine);
-
+				std::vector<int> numbers;
+				char* tok = strtok(thisLine, " ");
+				while (tok)
+				{
+					numbers.push_back(atoi(tok));
+					tok = strtok(nullptr, " ");
+				}
+				if (safe(numbers))
+					++part1;
+				if (safe2(numbers))
+					++part2;
 			}
 		}
 	}
@@ -37,7 +83,7 @@ void Process(const char* filename, int64 expectedPart1 = -1, int64 expectedPart2
 
 int main()
 {
-	Process("example.txt");
+	Process("example.txt", 2, 4);
 	Process("input.txt");
 
 	return 0;
