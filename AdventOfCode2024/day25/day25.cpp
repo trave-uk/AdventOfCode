ea43f47858f607e87995b0b24a0cb0a65aa9fda4
Dave Knight <trave.uk@gmail.com>
2024-12-25 22:48:21 +0000

    AoC 2024 day 25

--- AdventOfCode2024/day25/day25.cpp
+++ AdventOfCode2024/day25/day25.cpp
@@ -4,12 +4,68 @@
 
 #include "stdafx.h"
 
-void Process(const char* filename, int64 expectedPart1 = -1, int64 expectedPart2 = -1)
+struct KeyOrLock
 {
+	std::vector<std::string> block;
+	bool bKey;
+	std::array<int, 5> teeth;
+
+	void CountTeeth()
+	{
+		if (bKey)
+		{
+			std::reverse(block.begin(), block.end());
+		}
+		teeth.fill(-1);
+		for (std::string line : block)
+		{
+			int index = 0;
+			for (char col : line)
+			{
+				teeth[index++] += col == '#';
+			}
+		}
+		if (bKey)
+		{
+			std::reverse(block.begin(), block.end());
+		}
+	}
+
+	bool Fits(const KeyOrLock& other) const
+	{
+		for (int i = 0; i < 5; ++i)
+		{
+			if (teeth[i] + other.teeth[i] > 5)
+				return false;
+		}
+		return true;
+	}
+};
+
+using Blocks = std::vector<KeyOrLock>;
+
+int64 CountFittingKeysAndLocks(const Blocks& keys, const Blocks& locks)
+{
+	int64 count = 0;
+	for (const KeyOrLock& key : keys)
+	{
+		for (const KeyOrLock& lock : locks)
+		{
+			if (lock.Fits(key))
+				++count;
+		}
+	}
+	return count;
+}
+
+void Process(const char* filename, int64 expectedPart1 = -1)
+{
+	Blocks keys;
+	Blocks locks;
+	KeyOrLock block;
+
 	char* buffer = new char[65536];
 	FILE* fp = fopen(filename, "rt");
-	int64 part1 = 0;
-	int64 part2 = 0;
 	while (!feof(fp))
 	{
 		char* thisLine = fgets(buffer, 65536, fp);
@@ -18,26 +74,34 @@ void Process(const char* filename, int64 expectedPart1 = -1, int64 expectedPart2
 			thisLine[strcspn(thisLine, "\n\r")] = '\0';
 			if (*thisLine)
 			{
-				int pos = 0;
 				std::string line(thisLine);
+				assert(line.length() == 5);
+				block.block.push_back(line);
+			}
+			else
+			{
+				block.bKey = block.block[0][0] == '.';
+				block.CountTeeth();
+				if (block.bKey)
+					keys.push_back(block);
+				else
+					locks.push_back(block);
 
+				block = KeyOrLock();
 			}
 		}
 	}
 	fclose(fp);
 	delete[] buffer;
 
+	int64 part1 = CountFittingKeysAndLocks(keys, locks);
 	assert(expectedPart1 == -1 || expectedPart1 == part1);
 	printf("%s: Part 1: %lld\n", filename, part1);
-
-	assert(expectedPart2 == -1 || expectedPart2 == part2);
-	printf("%s: Part 2: %lld\n", filename, part2);
-
 }
 
 int main()
 {
-	Process("example.txt");
+	Process("example.txt", 3);
 	Process("input.txt");
 
 	return 0;
